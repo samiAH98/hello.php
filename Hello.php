68b8fe8ede72efac76b801a656b4c328e90fc11d
samiAH98 <122149435+samiAH98@users.noreply.github.com>
2023-10-22 20:09:40 +0200

    Create Hello.php

---
+++ Hello.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Src;
+
+
+class Hello
+{
+    public function talk() {
+        return " Hello world ";
+    }
+    static function world(){
+        return " SayHello ";
+    }
+}
